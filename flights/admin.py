d720b9b1935f9713521b6b8ae4976f7d0d1a3920
Pavel Martínek <martinek.pavel@oa-opava.cz>
2023-04-21 09:32:15 +0200

    flights app done

--- flights/admin.py
+++ flights/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Flight, Airport
-
+from .models import *
 # Register your models here.
 
 admin.site.register(Airport)
 admin.site.register(Flight)
-
+admin.site.register(Passenger)
